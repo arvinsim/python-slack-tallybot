557a50a97a76f48e652c42129e3fa068c12dffa2
Arthur Vincent Simon <developarvin@gmail.com>
2023-11-30 19:52:11 +0800

    Initialize Bolt
    Signed-off-by: Arthur Simon <developarvin@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
-SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.getenv('SLACK_BOT_TOKEN'))
 
-print('SLACK_BOT_TOKEN', SLACK_BOT_TOKEN)
-print('SLACK_APP_TOKEN', SLACK_APP_TOKEN)
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.getenv('SLACK_APP_TOKEN')).start()
